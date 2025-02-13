85a91fbbed672f2dda0ebd609f82f2d23a7a9a21
Adriana <isabelbriceno3110@gmail.com>
2025-02-13 13:32:45 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,9 +16,39 @@ class _HomePageState extends State<HomePage> {
         //icono de la izquierda
         leading: Icon(
           Icons.menu,
-          color: Colors.grey[800],),
+          color: Colors.grey[800],
+          ),
           actions: [
-            Icon(Icons.person)],
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+            ],
+      ),
+      body: Column(
+        children:[
+      //1.texto principal(Text)
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Row(
+          children: [
+            Text("I want to ",style: TextStyle(fontSize: 32),),
+            Text("Eat",style: TextStyle(
+              //tamano de letras
+              fontSize: 32,
+              //negritas
+              fontWeight: FontWeight.bold,
+              //subrayado
+              decoration: TextDecoration.underline,
+            ),
+            ),
+          ],
+        ),
+      )
+      //2.pestanas(TabBar)
+      //3.contenido de pestanas(TabBarView)
+      //4.carrito(cart)
+      ],
       ),
     );
   }
